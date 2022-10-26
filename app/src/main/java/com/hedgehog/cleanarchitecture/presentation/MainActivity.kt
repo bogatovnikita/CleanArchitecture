2e9977b773c0896c94e4410b00681c0d5e3c3939
bogatovnikita <bogatovnikita@gmail.com>
2022-10-26 17:07:25 +0300

    test

--- app/src/main/java/com/hedgehog/cleanarchitecture/presentation/MainActivity.kt
+++ app/src/main/java/com/hedgehog/cleanarchitecture/presentation/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initObservers()
